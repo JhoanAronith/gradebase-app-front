283bef6c358d4f6b7687175579fa41cf24a38e6d
JhoanAronith <jhoanmunozman@gmail.com>
2025-09-08 19:27:20 -0500

    Funcionalidad de la pagina de registro completa

---
+++ src/app/Componentes/register/register.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  templateUrl: './register.html',
+  styleUrls: ['./register.css']
+})
+export class Register {
+
+}
