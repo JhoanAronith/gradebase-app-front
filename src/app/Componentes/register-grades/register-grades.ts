67cda9987a2981e6b8050440ef763f1b5c6ce002
JhoanAronith <jhoanmunozman@gmail.com>
2025-09-08 20:05:23 -0500

    Diseño de la pagina de registro de notas completo

---
+++ src/app/Componentes/register-grades/register-grades.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-grades',
+  imports: [],
+  templateUrl: './register-grades.html',
+  styleUrl: './register-grades.css'
+})
+export class RegisterGrades {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './Componentes/login/login';
 import { Register } from './Componentes/register/register';
+import { RegisterGrades } from './Componentes/register-grades/register-grades';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'login', component: Login},
-  {path: 'register', component: Register}
+  {path: 'register', component: Register},
+  {path: 'registerGrades', component: RegisterGrades}
 ];
