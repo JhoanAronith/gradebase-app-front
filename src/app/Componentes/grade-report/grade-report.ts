3c5a1f32ce384c296b19665e51d71f50d700811f
JhoanAronith <jhoanmunozman@gmail.com>
2025-09-08 20:14:51 -0500

    Diseño de reporte e historial de notas completo

---
+++ src/app/Componentes/grade-report/grade-report.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-report',
+  imports: [],
+  templateUrl: './grade-report.html',
+  styleUrl: './grade-report.css'
+})
+export class GradeReport {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './Componentes/login/login';
 import { Register } from './Componentes/register/register';
 import { RegisterGrades } from './Componentes/register-grades/register-grades';
+import { GradeReport } from './Componentes/grade-report/grade-report';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'login', component: Login},
   {path: 'register', component: Register},
-  {path: 'registerGrades', component: RegisterGrades}
+  {path: 'registerGrades', component: RegisterGrades},
+  {path: 'gradeReport', component: GradeReport}
 ];
